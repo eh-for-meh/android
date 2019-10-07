c0c572c83736192690cc7afa9a32cd4e9ddefba6
Kirin Patel <kirinpatel@gmail.com>
2019-10-07 13:50:05 -0400

    Finished beta build

--- app/src/main/java/com/kirinpatel/ehformeh/activities/MainActivity.kt
+++ app/src/main/java/com/kirinpatel/ehformeh/activities/MainActivity.kt
@@ -6,14 +6,16 @@ import android.content.res.ColorStateList
 import android.graphics.Color
 import android.net.Uri
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.graphics.drawable.DrawableCompat
 import androidx.core.widget.ImageViewCompat
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.AdView
-import com.google.android.gms.ads.MobileAds
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 import com.kirinpatel.ehformeh.R
@@ -21,6 +23,9 @@ import com.kirinpatel.ehformeh.utils.Deal
 import com.kirinpatel.ehformeh.utils.Theme
 import com.squareup.picasso.Picasso
 import io.noties.markwon.Markwon
+import android.content.ClipData.newIntent
+
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -31,6 +36,8 @@ class MainActivity : AppCompatActivity() {
     lateinit var dealPriceTextView: TextView
     lateinit var dealFeaturesTextView: TextView
     lateinit var dealSpecificationsTextView: TextView
+    lateinit var dealStoryTextView: TextView
+    lateinit var theme: Theme
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,6 +50,7 @@ class MainActivity : AppCompatActivity() {
         dealPriceTextView = findViewById(R.id.dealPriceTextView)
         dealFeaturesTextView = findViewById(R.id.dealFeaturesTextView)
         dealSpecificationsTextView = findViewById(R.id.dealSpecificationsTextView)
+        dealStoryTextView = findViewById(R.id.dealStoryTextView)
 
         if (intent.extras != null && intent.extras[MainActivity.DEAL_KEY] != null) {
             val deal = intent.extras[MainActivity.DEAL_KEY] as Deal
@@ -63,6 +71,42 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.menu_main, menu)
+        if (theme != null) {
+            if (menu != null) {
+                for (i in 0 until menu.size()) {
+                    val drawable = menu.getItem(i).icon
+                    if (drawable != null) {
+                        val wrapped = DrawableCompat.wrap(drawable)
+                        drawable.mutate()
+                        DrawableCompat.setTint(wrapped, theme.backgroundColor)
+                        menu.getItem(i).icon = drawable
+                    }
+                }
+
+            }
+        }
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if (item == null) {
+            return super.onOptionsItemSelected(item)
+        }
+        return when (item.itemId) {
+            R.id.action_buy -> {
+                buyDeal()
+                true
+            }
+            R.id.action_settings -> {
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
     private fun setupTheme(theme: Theme) {
         val foreground = if (theme.isDark) Color.WHITE else Color.BLACK
         app_bar.setBackgroundColor(theme.accentColor)
@@ -71,6 +115,7 @@ class MainActivity : AppCompatActivity() {
         toolbar_layout.setCollapsedTitleTextColor(theme.backgroundColor)
         toolbar.setBackgroundColor(theme.accentColor)
         buyFab.backgroundTintList = ColorStateList.valueOf(foreground)
+        buyFab.setOnClickListener { buyDeal() }
         ImageViewCompat.setImageTintList(buyFab, ColorStateList.valueOf(theme.backgroundColor))
         backgroundConstraintLayout.setBackgroundColor(theme.backgroundColor)
         constraintLayout.setBackgroundColor(theme.backgroundColor)
@@ -78,6 +123,8 @@ class MainActivity : AppCompatActivity() {
         dealPriceTextView.setTextColor(theme.accentColor)
         dealFeaturesTextView.setTextColor(theme.accentColor)
         dealSpecificationsTextView.setTextColor(theme.accentColor)
+        dealStoryTextView.setTextColor(theme.accentColor)
+        this.theme = theme
     }
 
     private fun setupView(deal: Deal) {
@@ -93,6 +140,13 @@ class MainActivity : AppCompatActivity() {
         val markwon = Markwon.create(applicationContext)
         markwon.setMarkdown(dealFeaturesTextView, deal.features)
         markwon.setMarkdown(dealSpecificationsTextView, deal.specifications)
+        val story: String = StringBuilder()
+                .append("# ")
+                .append(deal.story.title)
+                .append("\n")
+                .append(deal.story.body)
+                .toString()
+        markwon.setMarkdown(dealStoryTextView, story)
     }
 
     private fun loadCurrentDeal() {
@@ -107,6 +161,13 @@ class MainActivity : AppCompatActivity() {
                     .setActionTextColor(Color.LTGRAY)
                     .show()
         }
+    }
+
+    private fun buyDeal() {
+        val url = "https://meh.com/account/signin?returnurl=https%3A%2F%2Fmeh.com%2F%23checkout"
+        val i = Intent(Intent.ACTION_VIEW)
+        i.data = Uri.parse(url)
+        startActivity(i)
     }
 
     companion object {
